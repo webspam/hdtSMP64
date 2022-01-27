webspam/hdtSMP64

commit 906e24be36c2c3a6d6de6767e1c72a34f064e618

- Major performance improvement: we now drop the FrameEvents if the lock   allowing to process them is already taken. Other events have to be   managed, the FrameEvent is the only event that we can drop, as we will   always have one later where we'll be able to manage the passed time.   As the lock is already taken, we would have to wait for the lock to be   free to process the event; we prefer to wait for the next event instead.   Moreover, dropping a locked part of the code allows to reduce the total   wait times.   Finally, some skse mods issue FrameEvents, this mechanism manages the   case where they issue too many.   This should allow a lot more stable performance, when too many   FrameEvents, when lots of skeletons entering or exiting the simulation,   when fps are too low.
- Major performance improvement: same logic now for the CUDA and not-CUDA
  code about calling the physics simulation. More readable. Correctly
  commented. Consistent with the fps. No magic value. Less calculus.
  Faster. Doesn't advance the simulation if less than half of the usual
  time has passed since last computation. Before, it was 1/4th of the
  usual time, which led to more simulation cost when rare early events.
  If the fps is greater than 60 (in fact the min-fps value configured in
  the xml file), there is one physics computation each frame; if it is
  lower, there is at least one computation per 1/60s to have a good
  physics simulation. There are maximum 3 steps of 1/60s each frame,
  so the physics remains correct until 20 fps as long as you have 20+
  fps. Before there were 5 steps, which was useless: it helped have a
  good physics simulation when you were at 12fps, by using more cpu when
  you were already starved of it. This code is a lot more stable as low
  fps (no rotating skeletons!), and has a lot less overhead at high fps.

- Faster getNpcNode function managing the lurker skeleton case.
- Probably useless micro-optimizations.

hdtSMP64/ActorManager.cpp
@@ -8,14 +8,12 @@
 #include <cinttypes>
 #include "Offsets.h"
 #include "skse64/GameStreams.h"
-#include <numeric>
 
 namespace hdt
 {
 	ActorManager::ActorManager()
 	{
 	}
-
 
 	ActorManager::~ActorManager()
 	{
@@ -44,36 +42,29 @@ namespace hdt
 	inline bool isFirstPersonSkeleton(NiNode* npc)
 	{
 		if (!npc) return false;
-		if (findNode(npc, "Camera1st [Cam1]")) return true;
-		return false;
+		return findNode(npc, "Camera1st [Cam1]") ? true : false;
 	}
 
 	NiNode* getNpcNode(NiNode* skeleton)
 	{
 		// TODO: replace this with a generic skeleton fixing configuration option
 		// hardcode an exception for lurker skeletons because they are made incorrectly
-		auto npc = findNode(skeleton, "NPC");
+		auto shouldFix = false;
 		if (skeleton->m_owner && skeleton->m_owner->baseForm)
 		{
 			auto npcForm = DYNAMIC_CAST(skeleton->m_owner->baseForm, TESForm, TESNPC);
-			if (npcForm && npcForm->race.race)
-			{
-				if (!strcmp(npcForm->race.race->models[0].GetModelName(),
-					"Actors\\DLC02\\BenthicLurker\\Character Assets\\skeleton.nif"))
-				{
-					npc = findNode(skeleton, "NPC Root [Root]");
-				}
-			}
+			if (npcForm && npcForm->race.race
+				&&!strcmp(npcForm->race.race->models[0].GetModelName(), "Actors\\DLC02\\BenthicLurker\\Character Assets\\skeleton.nif"))
+				shouldFix = true;
 		}
-		return npc;
+		return findNode(skeleton, shouldFix ? "NPC Root [Root]" : "NPC");
 	}
 
+	// TODO Shouldn't there be an ArmorDetachEvent?
 	void ActorManager::onEvent(const ArmorAttachEvent& e)
 	{
-		if (!findNode(e.skeleton, "NPC"))
-		{
-			return;
-		}
+		// No armor is ever attached to a lurker skeleton, thus we don't need to test.
+		if (!findNode(e.skeleton, "NPC")) return;
 
 		std::lock_guard<decltype(m_lock)> l(m_lock);
 		if (m_shutdown) return;
@@ -106,7 +97,15 @@ namespace hdt
 
 	void ActorManager::onEvent(const FrameEvent& e)
 	{
-		std::lock_guard<decltype(m_lock)> l(m_lock);
+		// Other events have to be managed. The FrameEvent is the only event that we can drop,
+		// we always have one later where we'll be able to manage the passed time.
+		// We drop this execution when the lock is already taken; in that case, we would execute the code later.
+		// It is better to drop it now, and let the next frame manage it.
+		// Moreover, dropping a locked part of the code allows to reduce the total wait times.
+		// Finally, some skse mods issue FrameEvents, this mechanism manages the case where they issue too many.
+		std::unique_lock<decltype(m_lock)> lock(m_lock, std::try_to_lock);
+		if (!lock.owns_lock()) return;
+
 		if (m_shutdown) return;
 
 		// We get the player character and its cell.
@@ -152,8 +151,8 @@ namespace hdt
 
 	void ActorManager::onEvent(const SkinSingleHeadGeometryEvent& e)
 	{
+		// This case never happens to a lurker skeleton, thus we don't need to test.
 		auto npc = findNode(e.skeleton, "NPC");
-
 		if (!npc) return;
 
 		std::lock_guard<decltype(m_lock)> l(m_lock);
@@ -177,6 +176,7 @@ namespace hdt
 				_DMESSAGE("renaming nodes in original part %s back", headPartIter->origPartRootNode->m_name);
 				for (auto& entry : skeleton.head.renameMap)
 				{
+					// This case never happens to a lurker skeleton, thus we don't need to test.
 					auto node = findNode(headPartIter->origPartRootNode, entry.second->cstr());
 					if (node)
 					{
@@ -194,8 +194,8 @@ namespace hdt
 
 	void ActorManager::onEvent(const SkinAllHeadGeometryEvent& e)
 	{
+		// This case never happens to a lurker skeleton, thus we don't need to test.
 		auto npc = findNode(e.skeleton, "NPC");
-
 		if (!npc) return;
 
 		std::lock_guard<decltype(m_lock)> l(m_lock);
@@ -284,7 +284,6 @@ namespace hdt
 		*/
 	}
 #endif
-
 	ActorManager::Skeleton& ActorManager::getSkeletonData(NiNode* skeleton)
 	{
 		auto iter = std::find_if(m_skeletons.begin(), m_skeletons.end(), [=](Skeleton& i)
@@ -335,6 +334,7 @@ namespace hdt
 				continue;
 			}
 
+			// TODO check it's not a lurker skeleton
 			auto dstChild = findNode(dst, srcChild->m_name);
 			if (dstChild)
 			{
@@ -397,22 +397,22 @@ namespace hdt
 		}
 	}
 
+	// returns the name of the skeleton owner
 	std::string ActorManager::Skeleton::name()
-		//return the name of the skeleton owner
 	{
-		auto name = "";
 		if (skeleton->m_owner && skeleton->m_owner->baseForm) {
 			auto bname = DYNAMIC_CAST(skeleton->m_owner->baseForm, TESForm, TESFullName);
 			if (bname)
-				name = bname->GetName();
+				return bname->GetName();
 		}
-		return name;
+		return "";
 	}
 
 	void ActorManager::Skeleton::addArmor(NiNode* armorModel)
 	{
 		IDType id = armors.size() ? armors.back().id + 1 : 0;
 		auto prefix = armorPrefix(id);
+		// FIXME we probably could simplify this by using findNode as surely we don't merge Armors with lurkers skeleton?
 		npc = getNpcNode(skeleton);
 		auto physicsFile = DefaultBBP::instance()->scanBBP(armorModel);
 
@@ -431,20 +431,18 @@ namespace hdt
 			_MESSAGE("Not attaching armor - no record or physics already exists");
 		}
 		Armor& armor = armors.back();
-
 		armor.armorWorn = attachedNode;
-		std::unordered_map<IDStr, IDStr> renameMap = armor.renameMap;
 
 		if (!isFirstPersonSkeleton(skeleton))
 		{
+			// FIXME we probably could simplify this by using findNode as surely we don't attach Armors to lurkers skeleton?
 			auto system = SkyrimSystemCreator().createSystem(getNpcNode(skeleton), attachedNode, armor.physicsFile,
-				std::move(renameMap));
+				std::move(armor.renameMap));
 
 			if (system)
 			{
 				armor.setPhysics(system, isActive);
 				hasPhysics = true;
-				armorMeshes += armor.meshes().size();
 			}
 		}
 	}
@@ -535,6 +533,7 @@ namespace hdt
 		armors.clear();
 	}
 
+	// Is called to print messages only
 	bool ActorManager::Skeleton::checkPhysics()
 	{
 		hasPhysics = false;
@@ -551,7 +550,6 @@ namespace hdt
 
 		return hasPhysics;
 	}
-
 
 	bool ActorManager::Skeleton::isActiveInScene() const
 	{
@@ -571,11 +569,9 @@ namespace hdt
 	{
 		if (npc)
 		{
+			// This works for lurker skeletons.
 			auto rootNode = findNode(npc, "NPC Root [Root]");
-			if (rootNode)
-			{
-				return rootNode->m_worldTransform.pos;
-			}
+			if (rootNode) return rootNode->m_worldTransform.pos;
 		}
 		return std::optional<NiPoint3>();
 	}
@@ -654,8 +650,6 @@ namespace hdt
 
 	void ActorManager::Skeleton::reloadMeshes()
 	{
-		armorMeshes = 0;
-		headMeshes = 0;
 		for (auto& i : armors)
 		{
 			i.clearPhysics();
@@ -670,7 +664,6 @@ namespace hdt
 				{
 					i.setPhysics(system, isActive);
 					hasPhysics = true;
-					armorMeshes += i.meshes().size();
 				}
 			}
 		}
@@ -679,15 +672,15 @@ namespace hdt
 
 	void ActorManager::Skeleton::scanHead()
 	{
-		if (isFirstPersonSkeleton(this->skeleton))
-		{
-			_DMESSAGE("not scanning head of first person skeleton");
-			return;
-		}
-
 		if (!this->head.headNode)
 		{
 			_DMESSAGE("actor has no head node");
+			return;
+		}
+
+		if (isFirstPersonSkeleton(this->skeleton))
+		{
+			_DMESSAGE("not scanning head of first person skeleton");
 			return;
 		}
 
@@ -724,7 +717,6 @@ namespace hdt
 				_DMESSAGE("success");
 				headPart.setPhysics(system, isActive);
 				hasPhysics = true;
-				headMeshes += headPart.meshes().size();
 			}
 		}
 	}
@@ -871,10 +863,12 @@ namespace hdt
 				auto obj = findObject(head.npcFaceGeomNode, geometry->m_name);
 				if (obj)
 				{
-					if (obj->GetAsBSGeometry())
-						origGeom = obj->GetAsBSGeometry();
-					else if (obj->GetAsNiGeometry())
-						origNiGeom = obj->GetAsNiGeometry();
+					auto ob = obj->GetAsBSGeometry();
+					if (ob) origGeom = ob;
+					else {
+						auto on = obj->GetAsNiGeometry();
+						if (on) origNiGeom = on;
+					}
 				}
 			}
 		}
@@ -914,6 +908,7 @@ namespace hdt
 				hasRenames = true;
 			}
 			
+			// TODO check it's not a lurker skeleton
 			auto boneNode = findNode(this->npc, boneName);
 
 			if (!boneNode && !hasMerged)
@@ -927,6 +922,7 @@ namespace hdt
 				{
 					// Facegen data doesn't have any tree structure to the skeleton. We need to make any new
 					// nodes children of the head node, so that they move properly when there's no physics.
+					// This case never happens to a lurker skeleton, thus we don't need to test.
 					auto headNode = findNode(head.npcFaceGeomNode, "NPC Head [Head]");
 					if (headNode)
 					{
@@ -935,6 +931,7 @@ namespace hdt
 						for (int i = 0; i < head.npcFaceGeomNode->m_children.m_arrayBufLen; ++i)
 						{
 							Ref<NiNode> child = castNiNode(head.npcFaceGeomNode->m_children.m_data[i]);
+							// This case never happens to a lurker skeleton, thus we don't need to test.
 							if (child && !findNode(npc, child->m_name))
 							{
 								child->m_localTransform = invTransform * child->m_localTransform;
@@ -955,7 +952,8 @@ namespace hdt
 					boneName = postMergeRenameIt->second->cstr();
 					hasRenames = true;
 				}
-				
+
+				// TODO check it's not a lurker skeleton
 				boneNode = findNode(this->npc, boneName);		
 			}
 
@@ -983,7 +981,6 @@ namespace hdt
 					{
 						findNode->second += 1;
 						_DMESSAGE("incrementing use count by 1, it is now %d", findNode->second);
-
 					}
 					else
 					{

hdtSMP64/ActorManager.h
@@ -108,7 +108,6 @@ namespace hdt
 			bool hasPhysics = false;
 			std::optional<NiPoint3> position() const;
 
-			/* Updates isActive, state, armors.isActive and	headParts.isActive. */
 			void updateAttachedState(NiPoint3 cameraPosition, float maxDistance, const NiNode* playerCell, NiPoint3 cameraOrientation, float maxAngleCosinus2);
 			void reloadMeshes();
 
@@ -126,8 +125,6 @@ namespace hdt
 		private:
 			bool isActiveInScene() const;
 			bool checkPhysics();
-			int headMeshes = 0;
-			int armorMeshes = 0;
 
 			bool isActive = false;
 			std::vector<Armor> armors;

hdtSMP64/hdtDefaultBBP.cpp
@@ -179,6 +179,7 @@ namespace hdt
 	DefaultBBP::NameMap DefaultBBP::defaultNameMap(NiNode* armor)
 	{
 		std::unordered_map<std::string, std::unordered_set<std::string> > nameMap;
+		// This case never happens to a lurker skeleton, thus we don't need to test.
 		auto skinned = findNode(armor, "BSFaceGenNiNodeSkinned");
 		if (skinned)
 		{

hdtSMP64/hdtSkinnedMesh/hdtCollider.cpp
@@ -140,7 +140,9 @@ namespace hdt
 		aabbAll = aabbMe;
 		for (auto& i : children)
 		{
+			// FIXME PROFILING Lots of time is used here, because this is called a lot.
 			i.updateAabb();
+
 			aabbAll.merge(i.aabbAll);
 		}
 	}

hdtSMP64/hdtSkinnedMesh/hdtSkinnedMeshAlgorithm.cpp
@@ -452,6 +452,7 @@ namespace hdt
 			};
 
 			if (pairs.size() >= std::thread::hardware_concurrency())
+				// FIXME PROFILING This is the line where we spend the most time in the whole mod.
 				concurrency::parallel_for_each(pairs.begin(), pairs.end(), func);
 			else for (auto& i : pairs) func(i);
 

hdtSMP64/hdtSkinnedMesh/hdtSkinnedMeshBody.cpp
@@ -136,6 +136,7 @@ __kernel void updateVertices(
 			m_bones[i].m_vertexToWorld.m_col[3][3] *= v.ptr->m_marginMultipler;
 		}
 	}
+
 	void SkinnedMeshBody::internalUpdate()
 	{
 		updateBones();
@@ -180,7 +181,9 @@ __kernel void updateVertices(
 			m_vpos[idx].set(posMargin);
 		}
 
+		// FIXME PROFILING Lots of times is spent here.
 		m_shape->internalUpdate();
+
 		m_bulletShape.m_aabb = m_shape->m_tree.aabbAll;
 	}
 #endif
@@ -265,17 +268,11 @@ __kernel void updateVertices(
 	{
 		if (m_isKinematic && body->m_isKinematic) return false;
 
-		if (m_canCollideWithTags.empty())
-		{
-			for (auto& i : body->m_tags)
-				if (m_noCollideWithTags.find(i) != m_noCollideWithTags.end())
-					return false;
-			return true;
-		}
+		auto res = m_canCollideWithTags.empty();
 		for (auto& i : body->m_tags)
 			if (m_canCollideWithTags.find(i) != m_canCollideWithTags.end())
-				return true;
-		return false;
+				return !res;
+		return res;
 	}
 
 	void SkinnedMeshBody::updateBoundingSphereAabb()

hdtSMP64/hdtSkinnedMesh/hdtSkinnedMeshWorld.cpp
@@ -65,7 +65,9 @@ namespace hdt
 		for (int i = 0; i < system->m_constraints.size(); ++i)
 			addConstraint(system->m_constraints[i]->m_constraint, true);
 
+		// -10 allows RESET_PHYSICS down the calls. But equality with a float?...
 		system->readTransform(-10.0);
+
 		system->m_world = this;
 	}
 
@@ -92,24 +94,20 @@ namespace hdt
 		system->m_world = nullptr;
 	}
 
-	int SkinnedMeshWorld::stepSimulation(btScalar timeStep, int maxSubSteps, btScalar fixedTimeStep)
+	int SkinnedMeshWorld::stepSimulation(btScalar remainingTimeStep, int maxSubSteps, btScalar fixedTimeStep)
 	{
-		if (timeStep > fixedTimeStep * maxSubSteps)
-			timeStep = fixedTimeStep * maxSubSteps;
-
 		applyGravity();
 
-		// [31/12/2021 DaydreamingDay] TODO what does this value mean? Make it configurable?
-#ifdef CUDA
-		while (timeStep >= fixedTimeStep * 1.5f)
-#else
-		while (timeStep >= fixedTimeStep * 1.25f)
-#endif
+		while (remainingTimeStep > fixedTimeStep)
 		{
 			internalSingleStepSimulation(fixedTimeStep);
-			timeStep -= fixedTimeStep;
+			remainingTimeStep -= fixedTimeStep;
 		}
-		internalSingleStepSimulation(timeStep);
+		// For the sake of the bullet library, we don't manage a step that would be lower than a 300Hz frame.
+		// Review this when (screens / Skyrim) will allow 300Hz+.
+		const auto minPossiblePeriod = 1.0f / 300.0f;
+		if (remainingTimeStep > minPossiblePeriod)
+			internalSingleStepSimulation(remainingTimeStep);
 		clearForces();
 
 		_bodies.clear();

hdtSMP64/hdtSkinnedMesh/hdtSkinnedMeshWorld.h
@@ -14,7 +14,7 @@ namespace hdt
 		virtual void addSkinnedMeshSystem(SkinnedMeshSystem* system);
 		virtual void removeSkinnedMeshSystem(SkinnedMeshSystem* system);
 
-		int stepSimulation(btScalar timeStep, int maxSubSteps = 1,
+		int stepSimulation(btScalar remainingTimeStep, int maxSubSteps = 1,
 		                   btScalar fixedTimeStep = btScalar(1.) / btScalar(60.)) override;
 
 		btVector3& getWind() { return m_windSpeed; }

commit c0d850243a3e1519a6b43d4795d58d0e30f17010

Bugfix: bug happened when attaching armor, for example when doing smp reset. Bug introduced in commit 906e24be36c2c3a6d6de6767e1c72a34f064e618.

hdtSMP64/ActorManager.cpp
@@ -435,9 +435,9 @@ namespace hdt
 
 		if (!isFirstPersonSkeleton(skeleton))
 		{
+			std::unordered_map<IDStr, IDStr> renameMap = armor.renameMap;
 			// FIXME we probably could simplify this by using findNode as surely we don't attach Armors to lurkers skeleton?
-			auto system = SkyrimSystemCreator().createSystem(getNpcNode(skeleton), attachedNode, armor.physicsFile,
-				std::move(armor.renameMap));
+			auto system = SkyrimSystemCreator().createSystem(getNpcNode(skeleton), attachedNode, armor.physicsFile, std::move(renameMap));
 
 			if (system)
 			{