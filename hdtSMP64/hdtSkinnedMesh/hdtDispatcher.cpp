webspam/hdtSMP64

commit 42b8e09272bf4edb040a563631c7d73df8d222d1

We're now able to build AE/SE, AVX/AVX2/AVX512/NoAVX, CUDA/NOCUDA from the same branch, and the same VS2019 solution. There is one project for each combination. CUDA specific code is between #ifdef CUDA. AE specific code is between #ifdef ANNIVERSARY_EDITION. hdtCudaCollision.cu and hdtCudaCollision.cuh are not built when in NOCUDA projects.

hdtSMP64/hdtSkinnedMesh/hdtDispatcher.cpp
@@ -1,16 +1,20 @@
 #include "hdtDispatcher.h"
 #include "hdtSkinnedMeshBody.h"
 #include "hdtSkinnedMeshAlgorithm.h"
+#ifdef CUDA
 #include "hdtCudaInterface.h"
 #include "hdtFrameTimer.h"
+#endif
 
 #include <LinearMath/btPoolAllocator.h>
 
+// #ifdef CUDA
 // If defined, triangle-vertex and vertex-vertex collision results aren't applied until the next frame. This
 // allows GPU collision detection to run concurrently with the rest of the game engine, instead of leaving
 // the CPU idle waiting for the results. Triangle-triangle collisions are assumed to require the higher
 // accuracy, and are always applied in the current frame.
 //#define CUDA_DELAYED_COLLISIONS
+// #endif
 
 namespace hdt
 {
@@ -69,52 +73,78 @@ namespace hdt
 
 		m_pairs.reserve(size);
 		auto pairs = pairCache->getOverlappingPairArrayPtr();
-
+#ifdef CUDA
 		using UpdateMap = std::unordered_map<SkinnedMeshBody*, std::pair<PerVertexShape*, PerTriangleShape*> >;
 		UpdateMap to_update;
-
 		// Find bodies and meshes that need collision checking. We want to keep them together in a map so they can
 		// be grouped by CUDA stream
 		for (int i = 0; i < size; ++i)
-		{
-			auto& pair = pairs[i];
-
-			auto shape0 = dynamic_cast<SkinnedMeshBody*>(static_cast<btCollisionObject*>(pair.m_pProxy0->m_clientObject));
-			auto shape1 = dynamic_cast<SkinnedMeshBody*>(static_cast<btCollisionObject*>(pair.m_pProxy1->m_clientObject));
-
-			if (shape0 || shape1)
+#else
+		SpinLock lock;
+		std::unordered_set<SkinnedMeshBody*> bodies;
+		std::unordered_set<PerVertexShape*> vertex_shapes;
+		std::unordered_set<PerTriangleShape*> triangle_shapes;
+		
+		concurrency::parallel_for(0, size, [&](int i)
+#endif
 			{
-				if (hdt::needsCollision(shape0, shape1) && shape0->isBoundingSphereCollided(shape1))
+				auto& pair = pairs[i];
+
+				auto shape0 = dynamic_cast<SkinnedMeshBody*>(static_cast<btCollisionObject*>(pair.m_pProxy0->m_clientObject));
+				auto shape1 = dynamic_cast<SkinnedMeshBody*>(static_cast<btCollisionObject*>(pair.m_pProxy1->m_clientObject));
+
+				if (shape0 || shape1)
 				{
-					auto it0 = to_update.insert({ shape0, {nullptr, nullptr} }).first;
-					auto it1 = to_update.insert({ shape1, {nullptr, nullptr} }).first;
-
-					m_pairs.push_back(std::make_pair(shape0, shape1));
-
-					auto a = shape0->m_shape->asPerTriangleShape();
-					auto b = shape1->m_shape->asPerTriangleShape();
-					if (a)
-						it0->second.second = a;
-					else
-						it0->second.first = shape0->m_shape->asPerVertexShape();
-					if (b)
-						it1->second.second = b;
-					else
-						it1->second.first = shape1->m_shape->asPerVertexShape();
-					if (a && b)
+					if (hdt::needsCollision(shape0, shape1) && shape0->isBoundingSphereCollided(shape1))
 					{
-						it0->second.first = a->m_verticesCollision;
-						it1->second.first = b->m_verticesCollision;
+#ifdef CUDA
+						auto it0 = to_update.insert({ shape0, {nullptr, nullptr} }).first;
+						auto it1 = to_update.insert({ shape1, {nullptr, nullptr} }).first;
+#else
+						HDT_LOCK_GUARD(l, lock);
+						bodies.insert(shape0);
+						bodies.insert(shape1);
+#endif
+						m_pairs.push_back(std::make_pair(shape0, shape1));
+
+						auto a = shape0->m_shape->asPerTriangleShape();
+						auto b = shape1->m_shape->asPerTriangleShape();
+#ifdef CUDA
+						if (a)
+							it0->second.second = a;
+						else
+							it0->second.first = shape0->m_shape->asPerVertexShape();
+						if (b)
+							it1->second.second = b;
+						else
+							it1->second.first = shape1->m_shape->asPerVertexShape();
+						if (a && b)
+						{
+							it0->second.first = a->m_verticesCollision;
+							it1->second.first = b->m_verticesCollision;
+						}
+#else
+						if (a)
+							triangle_shapes.insert(a);
+						else
+							vertex_shapes.insert(shape0->m_shape->asPerVertexShape());
+						if (b)
+							triangle_shapes.insert(b);
+						else
+							vertex_shapes.insert(shape0->m_shape->asPerVertexShape());
+						if (a && b)
+						{
+							vertex_shapes.insert(a->m_verticesCollision);
+							vertex_shapes.insert(b->m_verticesCollision);
+						}
+#endif
 					}
 				}
+				else getNearCallback()(pair, *this, dispatchInfo);
+#ifdef CUDA
 			}
-			else getNearCallback()(pair, *this, dispatchInfo);
-		}
-
 		bool haveCuda = CudaInterface::instance()->hasCuda() && (!FrameTimer::instance()->running() || FrameTimer::instance()->cudaFrame());
-
 		FrameTimer::instance()->logEvent(FrameTimer::e_Start);
-
 		if (haveCuda)
 		{
 			bool initialized = true;
@@ -139,22 +169,22 @@ namespace hdt
 			if (!initialized)
 			{
 				concurrency::parallel_for_each(to_update.begin(), to_update.end(), [deviceId](UpdateMap::value_type& o)
-				{
-					CudaInterface::instance()->setCurrentDevice();
+					{
+						CudaInterface::instance()->setCurrentDevice();
 
-					if (!o.first->m_cudaObject || o.first->m_cudaObject->deviceId() != deviceId)
-					{
-						o.first->m_cudaObject.reset(new CudaBody(o.first));
-					}
-					if (o.second.first && (!o.second.first->m_cudaObject || o.second.first->m_cudaObject->deviceId() != deviceId))
-					{
-						o.second.first->m_cudaObject.reset(new CudaPerVertexShape(o.second.first));
-					}
-					if (o.second.second && (!o.second.second->m_cudaObject || o.second.second->m_cudaObject->deviceId() != deviceId))
-					{
-						o.second.second->m_cudaObject.reset(new CudaPerTriangleShape(o.second.second));
-					}
-				});
+						if (!o.first->m_cudaObject || o.first->m_cudaObject->deviceId() != deviceId)
+						{
+							o.first->m_cudaObject.reset(new CudaBody(o.first));
+						}
+						if (o.second.first && (!o.second.first->m_cudaObject || o.second.first->m_cudaObject->deviceId() != deviceId))
+						{
+							o.second.first->m_cudaObject.reset(new CudaPerVertexShape(o.second.first));
+						}
+						if (o.second.second && (!o.second.second->m_cudaObject || o.second.second->m_cudaObject->deviceId() != deviceId))
+						{
+							o.second.second->m_cudaObject.reset(new CudaPerTriangleShape(o.second.second));
+						}
+					});
 			}
 
 			// FIXME: This is probably broken if the current CUDA device changes and any tasks haven't finished yet.
@@ -193,18 +223,18 @@ namespace hdt
 		else
 		{
 			concurrency::parallel_for_each(to_update.begin(), to_update.end(), [](UpdateMap::value_type& o)
-			{
-				o.first->internalUpdate();
-				if (o.second.first)
 				{
-					o.second.first->internalUpdate();
-				}
-				if (o.second.second)
-				{
-					o.second.second->internalUpdate();
-				}
-				o.first->m_bulletShape.m_aabb = o.first->m_shape->m_tree.aabbAll;
-			});
+					o.first->internalUpdate();
+					if (o.second.first)
+					{
+						o.second.first->internalUpdate();
+					}
+					if (o.second.second)
+					{
+						o.second.second->internalUpdate();
+					}
+					o.first->m_bulletShape.m_aabb = o.first->m_shape->m_tree.aabbAll;
+				});
 		}
 
 		FrameTimer::instance()->logEvent(FrameTimer::e_Internal);
@@ -261,12 +291,12 @@ namespace hdt
 			// Now we can process the collisions
 			concurrency::parallel_for_each(m_pairs.begin(), m_pairs.end(),
 				[this](std::pair<SkinnedMeshBody*, SkinnedMeshBody*>& i)
-			{
-				if (i.first->m_shape->m_tree.collapseCollideL(&i.second->m_shape->m_tree))
 				{
-					SkinnedMeshAlgorithm::processCollision(i.first, i.second, this);
-				}
-			});
+					if (i.first->m_shape->m_tree.collapseCollideL(&i.second->m_shape->m_tree))
+					{
+						SkinnedMeshAlgorithm::processCollision(i.first, i.second, this);
+					}
+				});
 			FrameTimer::instance()->logEvent(FrameTimer::e_Launched);
 		}
 
@@ -275,6 +305,32 @@ namespace hdt
 		FrameTimer::instance()->addManifoldCount(getNumManifolds());
 		FrameTimer::instance()->logEvent(FrameTimer::e_End);
 	}
+#else
+			});
+		concurrency::parallel_for_each(bodies.begin(), bodies.end(), [](SkinnedMeshBody* shape)
+		{
+			shape->internalUpdate();
+		});
+		concurrency::parallel_for_each(vertex_shapes.begin(), vertex_shapes.end(), [](PerVertexShape* shape)
+		{
+			shape->internalUpdate();
+		});
+		concurrency::parallel_for_each(triangle_shapes.begin(), triangle_shapes.end(), [](PerTriangleShape* shape)
+		{
+			shape->internalUpdate();
+		});
+		for (auto body : bodies)
+		{
+			body->m_bulletShape.m_aabb = body->m_shape->m_tree.aabbAll;
+		}
+		concurrency::parallel_for_each(m_pairs.begin(), m_pairs.end(), [&, this](const std::pair<SkinnedMeshBody*, SkinnedMeshBody*>& i)
+		{
+			if (i.first->m_shape->m_tree.collapseCollideL(&i.second->m_shape->m_tree))
+				SkinnedMeshAlgorithm::processCollision(i.first, i.second, this);
+		});	
+		m_pairs.clear();
+	}
+#endif
 
 	int CollisionDispatcher::getNumManifolds() const
 	{

hdtSMP64/config.cpp
@@ -2,7 +2,9 @@
 #include "XmlReader.h"
 
 #include "hdtSkyrimPhysicsWorld.h"
+#ifdef CUDA
 #include "hdtSkinnedMesh/hdtCudaInterface.h"
+#endif
 
 #include <clocale>
 
@@ -77,6 +79,7 @@ namespace hdt
 					SkyrimPhysicsWorld::get()->m_unclampedResetAngle = reader.readFloat();
 				else if (reader.GetLocalName() == "maximumDistance")
 					ActorManager::instance()->m_maxDistance = reader.readFloat();
+#ifdef CUDA
 				else if (reader.GetLocalName() == "enableCuda")
 					CudaInterface::enableCuda = reader.readBool();
 				else if (reader.GetLocalName() == "cudaDevice")
@@ -85,6 +88,7 @@ namespace hdt
 					if (device >= 0 && device < CudaInterface::instance()->deviceCount())
 						CudaInterface::currentDevice = device;
 				}
+#endif
 				else if (reader.GetLocalName() == "maximumAngle")
 					ActorManager::instance()->m_maxAngle = reader.readFloat();
 				else

hdtSMP64/hdtSMP64.vcxproj.filters
@@ -830,6 +830,17 @@
     <ClInclude Include="btBulletDynamicsCommon.h">
       <Filter>Header Files</Filter>
     </ClInclude>
+    <ClInclude Include="hdtSkinnedMesh\hdtCudaCollision.cuh" />
+    <ClInclude Include="hdtSkinnedMesh\hdtCudaInterface.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="hdtSkinnedMesh\hdtCudaPlanarStruct.cuh" />
+    <ClInclude Include="hdtSkinnedMesh\hdtFrameTimer.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="hdtSkinnedMesh\hdtLCP.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="hdtSkinnedMesh\hdtAabb.cpp">
@@ -1384,10 +1395,27 @@
     <ClCompile Include="BulletCollision\CollisionShapes\btMiniSDF.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>
+    <ClCompile Include="hdtSkinnedMesh\hdtCudaInterface.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="hdtSkinnedMesh\hdtFrameTimer.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="hdtSkinnedMesh\hdtLCP.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <None Include="export.def">
       <Filter>Source Files</Filter>
     </None>
+    <None Include="hdtSkinnedMesh\clkernel\updateTriShape.cl" />
+    <None Include="hdtSkinnedMesh\clkernel\updateTShape.cl" />
+    <None Include="hdtSkinnedMesh\clkernel\updateVertices.cl" />
+    <None Include="hdtSkinnedMesh\clkernel\updateVShape.cl" />
+    <None Include="hdtSkinnedMesh\clkernel\updateVtxShape.cl" />
+  </ItemGroup>
+  <ItemGroup>
+    <CudaCompile Include="hdtSkinnedMesh\hdtCudaCollision.cu" />
   </ItemGroup>
 </Project>

hdtSMP64/hdtSkinnedMesh/hdtBone.h
@@ -6,7 +6,16 @@ namespace hdt
 {
 	_CRT_ALIGN(16) struct Bone
 	{
+#ifndef CUDA
+		Bone() { _mm_store_ps(m_reserved, _mm_setzero_ps()); }
+#endif
+
 		// cache from rigidbody
 		btMatrix4x3T m_vertexToWorld;
+
+#ifndef CUDA
+		float m_reserved[3]; // reserved for float4 aligned
+		float m_maginMultipler; // scaled margin
+#endif
 	};
 }

hdtSMP64/hdtSkinnedMesh/hdtCollider.cpp
@@ -130,10 +130,12 @@ namespace hdt
 				aabb.merge(*i);
 			aabbMe = aabb;
 		}
+#ifdef CUDA
 		else
 		{
 			aabbMe.invalidate();
 		}
+#endif
 
 		aabbAll = aabbMe;
 		for (auto& i : children)
@@ -252,11 +254,12 @@ namespace hdt
 		for (auto& i : children)
 			i.remapColliders(start, startAabb);
 	}
-
+#ifdef CUDA
 	void ColliderTree::relocateAabb(Aabb* newAabb)
 	{
 		for (auto& i : children)
 			i.relocateAabb(newAabb + (i.aabb - aabb));
 		aabb = newAabb;
 	}
+#endif
 }

hdtSMP64/hdtSkinnedMesh/hdtCollider.h
@@ -67,7 +67,9 @@ namespace hdt
 		void insertCollider(const std::vector<U32>& keys, const Collider& c);
 		void exportColliders(vectorA16<Collider>& exportTo);
 		void remapColliders(Collider* start, Aabb* startAabb);
+#ifdef CUDA
 		void relocateAabb(Aabb* newAabb);
+#endif
 
 		void checkCollisionL(ColliderTree* r, std::vector<std::pair<ColliderTree*, ColliderTree*>>& ret);
 		void checkCollisionR(ColliderTree* r, std::vector<std::pair<ColliderTree*, ColliderTree*>>& ret);

commit cbb823f0c98ac6ed3790f8d2eada98173b758cc6

Bugfix: very probably made tits move too much. Bug introduced in 42b8e09272bf4edb040a563631c7d73df8d222d1

hdtSMP64/hdtSkinnedMesh/hdtDispatcher.cpp
@@ -131,7 +131,7 @@ namespace hdt
 						if (b)
 							triangle_shapes.insert(b);
 						else
-							vertex_shapes.insert(shape0->m_shape->asPerVertexShape());
+							vertex_shapes.insert(shape1->m_shape->asPerVertexShape());
 						if (a && b)
 						{
 							vertex_shapes.insert(a->m_verticesCollision);