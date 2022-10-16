9512a08dbf9201b8bff1402b27e2659a6d774531
jgernandt <jonas.gernandt@gmail.com>
2022-10-16 18:46:09 +0200

    Send simulation update events to plugins

--- hdtSMP64/hdtSkyrimPhysicsWorld.cpp
+++ hdtSMP64/hdtSkyrimPhysicsWorld.cpp
@@ -1,6 +1,8 @@
 #include "hdtSkyrimPhysicsWorld.h"
 #include <ppl.h>
 #include "Offsets.h"
+#include "PluginInterfaceImpl.h"
+
 #include "skse64/GameMenus.h"
 
 namespace hdt
@@ -91,11 +93,17 @@ namespace hdt
 				const auto remainingTimeStep = std::min(m_accumulatedInterval, tick * m_maxSubSteps);
 
 				readTransform(remainingTimeStep);
+
+				g_pluginInterface.onPreStep({ this, remainingTimeStep});
+
 				updateActiveState();
 				auto offset = applyTranslationOffset();
 				stepSimulation(remainingTimeStep, 0/*=maxSubSteps, ignored*/, tick);
 				restoreTranslationOffset(offset);
 				m_accumulatedInterval = 0;
+
+				g_pluginInterface.onPostStep({ this, remainingTimeStep });
+
 				writeTransform();
 			}
 		}
