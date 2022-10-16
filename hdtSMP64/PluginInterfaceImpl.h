93ebe5bd7b28abb97058c7f4f01c90a12531f741
jgernandt <jonas.gernandt@gmail.com>
2022-10-16 18:37:22 +0200

    Implement the plugin interface

---
+++ hdtSMP64/PluginInterfaceImpl.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "skse64/PluginAPI.h"
+
+#include "EventDispatcherImpl.h"
+#include "PluginAPI.h"
+
+namespace hdt
+{
+	class PluginInterfaceImpl final : public PluginInterface
+	{
+	public:
+		PluginInterfaceImpl() = default;
+		~PluginInterfaceImpl() = default;
+
+		PluginInterfaceImpl(const PluginInterfaceImpl&) = delete;
+		PluginInterfaceImpl& operator=(const PluginInterfaceImpl&) = delete;
+
+		virtual const VersionInfo& getVersionInfo() const { return m_versionInfo; }
+
+		virtual void addListener(IPreStepListener* l) override;
+		virtual void removeListener(IPreStepListener* l) override;
+
+		virtual void addListener(IPostStepListener* l) override;
+		virtual void removeListener(IPostStepListener* l) override;
+
+		void onPostPostLoad();
+
+		void onPreStep(const PreStepEvent& e) { m_preStepDispatcher.dispatch(e); }
+		void onPostStep(const PostStepEvent& e) { m_postStepDispatcher.dispatch(e); }
+
+		void init(const SKSEInterface* skse);
+
+	private:
+		VersionInfo m_versionInfo{ INTERFACE_VERSION, BULLET_VERSION };
+		EventDispatcherImpl<PreStepEvent> m_preStepDispatcher;
+		EventDispatcherImpl<PostStepEvent> m_postStepDispatcher;
+
+		PluginHandle m_sksePluginHandle;
+		SKSEMessagingInterface* m_skseMessagingInterface;
+	};
+
+	extern PluginInterfaceImpl g_pluginInterface;
+}
