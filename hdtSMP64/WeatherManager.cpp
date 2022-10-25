webspam/hdtSMP64

commit 39418aa4ac17661f3083cb0edaf3252e43453df3

fix: adapt wind code for Skyrim VR

hdtSMP64/WeatherManager.cpp
@@ -98,7 +98,12 @@ void hdt::WeatherCheck()
 			continue;
 		}
 
-		if (!(cell->worldSpace)) //Interior cell
+#ifdef SKYRIMVR
+		TESWorldSpace* worldSpace = cell->unk120;
+#else
+		TESWorldSpace* worldSpace = cell->worldspace;
+#endif
+		if (!worldSpace) // Interior cell
 		{
 			//LOG("In interior cell. Waiting for 5 seconds");
 			world->setWind(&NiPoint3{ 0,0,0 }, 0, 1); // remove wind immediately
@@ -107,7 +112,7 @@ void hdt::WeatherCheck()
 		}
 		else
 		{
-			if (std::find(notExteriorWorlds.begin(), notExteriorWorlds.end(), cell->worldSpace->formID) != notExteriorWorlds.end())
+			if (std::find(notExteriorWorlds.begin(), notExteriorWorlds.end(), worldSpace->formID) != notExteriorWorlds.end())
 			{
 				//LOG("In interior cell world. Waiting for 5 seconds");
 				world->setWind(&NiPoint3{ 0,0,0 }, 0, 1); // remove wind immediately
@@ -125,20 +130,12 @@ void hdt::WeatherCheck()
 			if (skyPtr->currentWeather)
 			{
 				_MESSAGE("Wind Speed: %2.2g, Wind Direction: %2.2g, Weather Wind Speed: %2.2g WindDir:%2.2g WindDirRange:%2.2g", skyPtr->windSpeed, skyPtr->windDirection,
-#ifndef SKYRIMVR
 					skyPtr->currentWeather->general.windSpeed, skyPtr->currentWeather->general.windDirection * 180.0f / 256.0f, skyPtr->currentWeather->general.windDirRange * 360.0f / 256.0f
-#else
- 					skyPtr->currentWeather->data.windSpeed, skyPtr->currentWeather->data.windDirection * 180.0f / 256.0f, skyPtr->currentWeather->data.windDirectionRange * 360.0f / 256.0f
-#endif
 				);
 				// use weather wind info
 				//Wind Speed is the only thing that changes. Wind direction and range are same all the time as set in CK.
 				const float theta = (((
-#ifndef SKYRIMVR
 					skyPtr->currentWeather->general.windDirection
-#else
-					skyPtr->currentWeather->data.windDirection
-#endif
 					) * 180.0f) / 256.0f) - 90.f + randomGenerator(-range, range);
 				precipDirection = rotate(precipDirection, NiPoint3(0, 0, 1.0f), theta / 57.295776f);
 				world->setWind(&precipDirection, world->m_windStrength * scaleSkyrim * skyPtr->windSpeed);

commit 143eca9a109ae261636ecce3beb7a557aa96a067

fix: fix typo introduced in 39418aa4ac17661f3083cb0edaf3252e43453df3

hdtSMP64/WeatherManager.cpp
@@ -101,7 +101,7 @@ void hdt::WeatherCheck()
 #ifdef SKYRIMVR
 		TESWorldSpace* worldSpace = cell->unk120;
 #else
-		TESWorldSpace* worldSpace = cell->worldspace;
+		TESWorldSpace* worldSpace = cell->worldSpace;
 #endif
 		if (!worldSpace) // Interior cell
 		{